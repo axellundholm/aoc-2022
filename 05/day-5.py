a54b3df27ed9c483f860e179f0b9fbf8e98e4f66
axellundholm <axel.lundholm@me.com>
2022-12-05 22:41:31 +0100

    complete solution for day five

--- 05/day-5.py
+++ 05/day-5.py
@@ -1,14 +1,49 @@
+import numpy as np
+
+def cleanup_input(lst):
+		
+	stacks = []
+
+	for x in reversed(lst[0:lst.index('')][:-1]):
+		stacks.append([*x[1:-1:4]])
+
+	stacks = np.array(stacks).T.tolist()
+	
+	for s in stacks:
+		while s[-1] == ' ':
+			s.pop()
+
+	inst = [[int(i) for i in x.split() if i.isdigit()] for x in lst[lst.index('') + 1:]]
+
+	return stacks, inst
+
+def move_crates(stacks, inst, model):
+	
+	for i in inst:
+		if model == '9000':
+			for r in range(i[0]):
+				stacks[i[2] - 1].append(stacks[i[1] - 1].pop())
+
+		elif model == '9001':
+			crane = []
+			
+			for r in range(i[0]):
+				crane.append(stacks[i[1] - 1].pop())
+			for c in reversed(crane):
+				stacks[i[2] - 1].append(c)
+
+	return ''.join([x.pop() for x in stacks])
 
 def main():
-	f = open('test-input', 'r')
+	f = open('input', 'r')
 	
 	l = f.readlines()
-	l = [x.strip() for x in l]
+	l = [x.strip('\n') for x in l]
 
-	print(l)
-
-	print("Answer to Part One: " + str('N/A'))
-	print("Answer to Part Two: " + str('N/A'))
+	stacks, inst = cleanup_input(l)
+	print("Answer to Part One: " + str(move_crates(stacks, inst, '9000')))
+	stacks, inst = cleanup_input(l)
+	print("Answer to Part Two: " + str(move_crates(stacks, inst, '9001')))
 
 
 if __name__=="__main__":
