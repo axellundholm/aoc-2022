66460c5e9cbc928dde186e58844afe8c45cd1281
axellundholm <axel.lundholm@me.com>
2022-12-06 08:12:28 +0100

    complete solution for day six

--- 06/day-6.py
+++ 06/day-6.py
@@ -1,14 +1,18 @@
 
+def find_start_marker(lst, nbr):
+
+	for x in range(len(lst) - nbr - 1):
+		if len(set(lst[x:x + nbr])) == nbr:
+			return x + nbr
+
 def main():
-	f = open('test-input', 'r')
+	f = open('input', 'r')
 	
 	l = f.readlines()
-	l = [x.strip() for x in l]
+	l = [x.strip() for x in l][0]
 
-	print(l)
-
-	print("Answer to Part One: " + str('N/A'))
-	print("Answer to Part Two: " + str('N/A'))
+	print("Answer to Part One: " + str(find_start_marker(l, 4)))
+	print("Answer to Part Two: " + str(find_start_marker(l, 14)))
 
 
 if __name__=="__main__":
