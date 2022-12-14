1bf508e8175db3dd5deb8b1e34889c6c8b9a4597
axellundholm <axel.lundholm@me.com>
2022-12-14 09:43:21 +0100

    prepare for day twelve

---
+++ 12/day-12.py
@@ -0,0 +1,17 @@
+
+def main():
+	f = open('test-input', 'r')
+	
+	l = f.readlines()
+	l = [x.strip() for x in l]
+
+	print(l)
+
+	print("Answer to Part One: " + str('N/A'))
+	print("Answer to Part Two: " + str('N/A'))
+
+
+if __name__=="__main__":
+	main()
+
+
